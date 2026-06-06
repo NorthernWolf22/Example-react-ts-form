0df8b6ac47da71c928d4a9fc2ccacc2453a97906
Clare Knight <clare.knight@northernwolf.marketing>
2026-06-06 17:31:35 +0100

    created custom Student type, a students state and new function to add a student to the array state

---
+++ src/types/student-types.ts
@@ -0,0 +1,10 @@
+/*
+This defines a custom TS type called student. A student object must have these 4 properties all of which must be strings.
+*/
+
+export type Student = {
+    id: string;
+    firstName: string;
+    lastName: string;
+    email: string;
+}
